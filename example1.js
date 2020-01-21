53d5e309fcf755b46d87a8d2f99cfb00fd0fdbd9
Angel España <a.espaben@gmail.com>
2020-01-21 16:21:13 -0430

    example1.js

--- example1.js
+++ example1.js
@@ -1,9 +1,17 @@
 const { parseInput, PARSER } = require("./ULA_parser");
 
 /* Modifíca la cadena para que pertenezca al lenguaje. */
-let code = 'CREA a = a + 5;';
+let code = `
+CREA a = a + 5;
+
+a = b;
+
+SI(a ES b) ENTONCES {
+  b = a;
+}
+`;
 
 
 let message = parseInput(code);
 
-console.log(message.children);
+console.log(message.children.Decision[0].children);
