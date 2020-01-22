e9aface2fdf51f9c1eeb34ca9692a0a6ec407301
Angel España <a.espaben@gmail.com>
2020-01-21 19:49:32 -0430

    Se comienza a programar el CST Visitor.

---
+++ ULA_CST.js
@@ -0,0 +1,18 @@
+/* CST Visitor.
+***********************************************/
+
+const { LEXER } = require("./ULA_lexer");
+const parser = require("./ULA_parser");
+const ULAParser = parser.ULAParser;
+const PARSER = new ULAParser([]);
+const BaseULAVisitor = PARSER.getBaseCstVisitorConstructor();
+
+
+class ULAtoAstVisitor extends BaseULAVisitor {
+  constructor() {
+    super();
+    this.validateVisitor();
+  }
+
+  /* Aquí comienzan las reglas de construcción del AST... */
+}
